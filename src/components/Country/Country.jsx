95c2aef55180e7d9f6823b0a6e50c3f3a5123466
Habibur Rahman <habibur88454@gmail.com>
2024-10-20 16:59:31 +0600

    visited btn added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,16 +1,34 @@
-
+import { useState } from "react";
+import './Country.css'
 const Country = ({ country }) => {
-    const { name, flags } = country;
-    console.log(country);
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited)
+    }
+
+    const { name, flags, capital } = country;
+    // console.log(capital[0]);
     const countryStyle = {
         border: '2px solid salmon',
         padding: '15px',
         borderRadius: '20px'
     }
     return (
-        <div style={countryStyle}>
-            <img src={flags?.png} alt="" />
+        <div style={countryStyle} className={`${visited && 'visited'}`}>
+            <img style={{ height: '200px', width: "100%" }} src={flags?.png} alt="" />
             <h3>Name: {name?.common}</h3>
+            <p>Capital: {capital}</p>
+            <button onClick={handleVisited}>
+                {
+                    visited ? 'Visited' : 'Visit'
+                }
+            </button>
+            <p>
+                {
+                    visited && 'Visited Done'
+                }
+            </p>
         </div>
     );
 };
